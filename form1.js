8ee63b41b776778672033f84e53f2acdc3f9b65c
Dtcgovpass <134592163+Dtcgovpass@users.noreply.github.com>
2024-07-26 14:08:23 +0530

    Update form1.js

--- form1.js
+++ form1.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
          
         // Check if the mobile number field is wrong
-        if (mobileNumberInput.value !== "9005365320") {
+        if (mobileNumberInput.value !== "9279114834") {
             alert("mobile no. not registered With this passid .\nPlease try again");
             location.reload();
             return; // Stop the function execution here
